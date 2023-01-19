843714b37a7521e37a73bfeab75d727f812f40f8
Abel Martell <amartellram@gmail.com>
2023-01-18 21:34:23 -0500

    format

--- example/lib/addComponent.js
+++ example/lib/addComponent.js
@@ -1,6 +1,6 @@
 //Refirma Invoker necesita este div oara funcionar
-let componente=document.getElementById("addComponent")
-if(componente==null){
+let componente = document.getElementById("addComponent")
+if (componente == null) {
 
     let elemDiv = document.createElement('div');
     elemDiv.setAttribute("id", "addComponent");
